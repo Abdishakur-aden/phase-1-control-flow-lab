12f42c4393517221724e57555106cc3e28dbc719
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-03-19 14:30:45 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if (number <= 400 ) {
+    return 'This one is on me!';
+  }
+  else if (number < 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if(number < 2500 ) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else (number > 2501) 
+    return 'No can do.';
+  }
+      
+
+
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return 'Thank you so much.';    
+  }else if (tip === 'not as generous'){
+    return 'Thank you.';
+  }else {
+    return 'Bye.';
+  }
+}  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+console.log (scuberGreetingForFeet(2100));
+console.log (ternaryCheckCity(NYC));
